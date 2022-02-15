d52450d4db8345c26921adfab727623bd6de9efb
navaricardo <ricardo.a.nava@outlook.com>
2022-02-15 13:30:34 -0800

    chore: add backend seed for dummy data

---
+++ src/BACKEND.ts
@@ -0,0 +1,23 @@
+export interface ISeed {
+  description: string;
+  amount: number;
+  currency: string;
+}
+
+export const SEED = [
+  {
+    description: "Descripcion",
+    amount: 233.59,
+    currency: "MXN",
+  },
+  {
+    description: "Descripcion",
+    amount: 28.99,
+    currency: "USD",
+  },
+  {
+    description: "Descripcion",
+    amount: 123786.33,
+    currency: "COP",
+  },
+];
