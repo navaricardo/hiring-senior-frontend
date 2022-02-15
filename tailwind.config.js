d4313dca0c576f9974ee6b74b45bb895090d2fb2
navaricardo <ricardo.a.nava@outlook.com>
2022-02-15 10:36:49 -0800

    build: add tailwind to project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors,
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"), // import tailwind forms
+  ],
+};
