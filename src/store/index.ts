be216318f414eff966356b12fae3235066e0423f
navaricardo <ricardo.a.nava@outlook.com>
2022-02-23 18:42:56 -0800

    feat: persist state in local storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,29 @@
-import { atom } from "recoil";
+import { atom, AtomEffect, DefaultValue } from "recoil";
 import { IInvoice } from "../interfaces";
+
+// effects
+const localStorageEffect: <T>(key: string) => AtomEffect<T> =
+  (key: string) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue) => {
+      if (newValue instanceof DefaultValue) {
+        localStorage.removeItem(key);
+      } else {
+        localStorage.setItem(key, JSON.stringify(newValue));
+      }
+    });
+  };
 
 // Recoil states
 const invoiceListState = atom<Array<IInvoice>>({
   key: "invoiceListstate",
   default: [],
+  effects: [localStorageEffect<Array<IInvoice>>("invoice_list")],
 });
 
 const invoiceTotalState = atom<number>({
